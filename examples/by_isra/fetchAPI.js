9ef26a070bacaf2cb526bdced9dd38480ae85ea6
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-12-26 11:58:26 +0400

    added .js file to fetch api (#39)

---
+++ examples/by_isra/fetchAPI.js
@@ -0,0 +1,15 @@
+function fetchJSONData() {
+    fetch("https://www.swapi.tech/api/planets")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) =>
+            console.log(data))
+        .catch((error) =>
+            console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
