b8681b898297379454905e13075b3aa46334c8ac
isra <imorasbi@gmail.com>
2024-12-31 12:03:04 +0400

    implemented api functions

---
+++ examples/by_isra/insight-flow-api/src/main/java/com/insightflow/codearch/insight_flow_api/Model/Card.java
@@ -0,0 +1,27 @@
+package com.insightflow.codearch.insight_flow_api.Model;
+
+public class Card {
+    public String id;
+    public String content;
+
+    public Card(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
