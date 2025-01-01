ba5c267076b0a4086a7c9ce56e6948c251ae5f8b
Faisal-Abdo <145707185+Faisal-Abdo@users.noreply.github.com>
2025-01-01 11:18:30 +0400

    Added CRUD operations in backend for the cards (#52)

---
+++ examples/by_faisal_abdo/Backend/InsightFlow-API/src/main/java/com/InsightFlow/InsightFlow/InsightFlowApplication.java
@@ -0,0 +1,13 @@
+package com.InsightFlow.InsightFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightFlowApplication.class, args);
+    }
+
+}
