377a3881702408fe38b14199da73425134454df6
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-01-01 11:18:59 +0400

    mege feature-6-backend into main (#50)

---
+++ examples/by_alazhar/javaMvnProject/insight-flow-api/src/main/java/com/insightflow/insight_flow_api/InsightFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.insightflow.codearchs.insight_flow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightFlowApiApplication.class, args);
+	}
+
+}
