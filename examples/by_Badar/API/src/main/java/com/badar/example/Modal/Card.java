0334f0a11881f83bebec8e3200e651688594c6ad
Badar <bader5uk@hotmail.com>
2025-01-06 12:07:14 +0400

    Updated the UI

--- examples/by_Badar/API/src/main/java/com/badar/example/Modal/Card.java
+++ examples/by_Badar/API/src/main/java/com/badar/example/Modal/Card.java
@@ -3,10 +3,12 @@ package com.badar.example.Modal;
 public class Card {
     public String id;
     public String content;
+    public String cardCategory;
 
-    public Card(String id, String content) {
+    public Card(String id, String content, String cardCategory) {
         this.id = id;
         this.content = content;
+        this.cardCategory = cardCategory;
     }
 }
 
--- examples/by_Badar/api.js
+++ examples/by_Badar/api.js
@@ -1,80 +1,152 @@
-fetch("http://localhost:8080/card")
-.then((response) => response.json())
-.then((result) => {})
-.catch((error) => console.error(error));
-
-const selectorItem = document.getElementById("selectorPost");
-// adding to what went array and loop (start)
 const whatWentWellList = [];
-whatWentWellList.push("we had revisions");
-whatWentWellList.push("We had tasks that gave us a good idea about HTM");
-whatWentWellList.push("We are motivated");
-const wwcDivObj = document.getElementById("whatWentWellColumn");
-for(let i = 0; i<whatWentWellList.length; i++){
-    let newSpanObj = document.createElement("h3");
-    newSpanObj.innerHTML = whatWentWellList[i];
-    newSpanObj.className = "wentWellobject";
-    
-    wwcDivObj.append(newSpanObj);
-}
-// adding to what went array and loop (end)
-
-// adding to challenges array and loop (start)
 const challengesList = [];
-challengesList.push("Time restrictions");
-challengesList.push("Focusing at the end of the session");
-const challengesDivObj = document.getElementById("challengesColumn");
-for(let i = 0; i<challengesList.length; i++){
-    let newSpanObj = document.createElement("h3");
-    newSpanObj.innerHTML = challengesList[i];
-    newSpanObj.className = "ChallengesObject";
-    
-    challengesDivObj.append(newSpanObj);
-}
-// adding to challenges array and loop (end)
-
-
-// adding to action list array and loop (start)
 const actionList = [];
-actionList.push("Read more about CSS");
-actionList.push("work harder");
+const cardIdSet = new Set();
+const wwcDivObj = document.getElementById("whatWentWellColumn");
+const challengesDivObj = document.getElementById("challengesColumn");
 const actionDivObj = document.getElementById("actionListColumn");
-for(let i = 0; i<actionList.length; i++){
-    let newSpanObj = document.createElement("h3");
-    newSpanObj.innerHTML = actionList[i];
-    newSpanObj.className = "actionListObject";
+
+let selDropdownElemId = null;
+
+fetch("http://localhost:8080/card")
+    .then((response) => response.json())
+    .then((result) => {
+        result.forEach(element => {
+            if (cardIdSet.has(element.id)) {
+                console.error("Duplicate card. Skipping!");
+            } else if (element.content == '' || element.cardCategory == ''){
+                console.error("Null card, skipping!")
+            } else {
+                if (element.cardCategory == "wwOption") {
+
+                    whatWentWellList.push(element.content);
+                    const wwcDivObj = document.getElementById("whatWentWellColumn");
+                    let newSpanObj = document.createElement("h4");
+                    newSpanObj.setAttribute('id', element.id);
+                    newSpanObj.innerHTML = `${element.content} <img src="trash.png" class="delete-btn" onclick="deleteCard('${element.id}')" >`;
+                    newSpanObj.className = "wentWellobject";
+
+                    wwcDivObj.append(newSpanObj);
+                }
+                // adding to challenges array and loop (start)
+                else if (element.cardCategory == "cfOption") {
+
+                    challengesList.push(element.content);
+                    const challengesDivObj = document.getElementById("challengesColumn");
+
+                    let newSpanObj = document.createElement("h4");
+                    newSpanObj.setAttribute('id', element.id)
+                    newSpanObj.innerHTML = `${element.content} <img src="trash.png" class="delete-btn" onclick="deleteCard('${element.id}')" >`;
+                    newSpanObj.className = "ChallengesObject";
+
+                    challengesDivObj.append(newSpanObj);
+
+                    // adding to challenges array and loop (end)
+                }
+
+                // adding to action list array and loop (start)
+                else if (element.cardCategory == "aicOption") {
+
+                    actionList.push(element.content);
+                    const actionDivObj = document.getElementById("actionListColumn");
+                    let newSpanObj = document.createElement("h4");
+                    newSpanObj.id = crypto.randomUUID();
+                    newSpanObj.innerHTML = `${element.content} <img src="trash.png" class="delete-btn" onclick="deleteCard('${element.id}')" >`;
+                    newSpanObj.className = "actionListObject";
+                    actionDivObj.append(newSpanObj);
+
+                    // adding to action array and loop (end)
+                }
+                cardIdSet.add(element.id);
+            }
+        });
+    })
+    .catch((error) => console.error(error));
+
+
+
+const addMesssage = () => {
+    const inputValue = document.getElementById("input").value;
+
+
+    if(selDropdownElemId === null || inputValue === ''){
+
+    } else{
+        const newListItem = document.createElement("h4");
+
+        if (selDropdownElemId == "wwOption") {
+            newListItem.className = "wentWellobject";
+            newListItem.id = crypto.randomUUID();
+            const uuid = newListItem.id.toString();
+            newListItem.innerHTML = `${inputValue} <img src="trash.png" class="delete-btn" onclick="deleteCard('${uuid}')">`;
     
-    actionDivObj.append(newSpanObj);
-}
-// adding to action array and loop (end)
-
-
- const addMesssage = () => {
-    const newListItem = document.createElement("h3");
-    newListItem.innerHTML = document.getElementById("input").value;
+            wwcDivObj.append(newListItem);
     
-    if(checkIndex() == "What went Well"){
-    newListItem.className = "wentWellobject";
+        } else if (selDropdownElemId == "cfOption") {
+            newListItem.className = "ChallengesObject"
+            newListItem.id = crypto.randomUUID();
+            const uuid = newListItem.id.toString();
+            newListItem.innerHTML = `${inputValue} <img src="trash.png" class="delete-btn" onclick="deleteCard('${uuid}')">`;
     
-    wwcDivObj.append(newListItem);
+            challengesDivObj.append(newListItem);
+    
+        } else if (selDropdownElemId == "aicOption") {
+            newListItem.className = "actionListObject"
+            newListItem.id = crypto.randomUUID();
+            const uuid = newListItem.id.toString();
+            newListItem.innerHTML = `${inputValue} <img src="trash.png" class="delete-btn" onclick="deleteCard('${uuid}')">`;
+    
+            actionDivObj.append(newListItem);
+        }
+        const raw = JSON.stringify({
+            "id": newListItem.id,
+            "content": document.getElementById("input").value,
+            "cardCategory": selDropdownElemId,
+        });
+    
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+              },
+            body: raw,
+        };
 
-    } else if (checkIndex() == "Challenges Faced"){
-    newListItem.className = "ChallengesObject"
+        fetch("http://localhost:8080/card", requestOptions)
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
 
-    challengesDivObj.append(newListItem);
-
-    } else if(checkIndex() == "Action items"){
-    newListItem.className = "actionListObject"
-
-    actionDivObj.append(newListItem);
     }
+    
+    
 
- }
- const changeBoardName =() => {
+    
+
+
+
+}
+const changeBoardName = () => {
     const boardTitleObj = document.getElementById("codeArchitectsHeader");
     boardTitleObj.innerHTML = document.getElementById("input2").value;
- }
+}
 
- const checkIndex = () =>{
-    return selectedOption = selectorItem.options[selectorItem.selectedIndex].innerHTML
- }
+const checkIndex = (anchorElement) => {
+    selDropdownElemId = anchorElement.id;
+}
+
+const deleteCard = (id) => {
+    fetch(`http://localhost:8080/card/${id}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+
+    const itemTobeDelted = document.getElementById(id);
+    itemTobeDelted.remove();
+  }
+const requestOptions = {
+  method: "DELETE"
+};
+
+
+
