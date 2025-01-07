e2cfe70e83a4d68b3af14bb266be6819d471168b
Badar <bader5uk@hotmail.com>
2025-01-07 14:03:56 +0400

    Merging Feature/new UI changes into main (#59)
    * Updated the UI
    
    * further edited the UI
    
    * centred a few elements and refactored style
    
    * added bootstrap theme
    
    * Refactored style and code
    
    * Added text wrap fot the objects
    
    * more style changes
    
    * added webpage title and favicon
    
    * changed directory
    
    * changed directory

--- examples/by_Badar/API/src/main/java/com/badar/example/Controller/CardController.java
+++ examples/by_Badar/API/src/main/java/com/badar/example/Controller/CardController.java
@@ -4,20 +4,38 @@ import com.badar.example.Modal.Card;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/card")
 public class CardController {
-     ArrayList<Card> cardList = new ArrayList<>();
+     private CopyOnWriteArrayList<Card> cardList = new CopyOnWriteArrayList<>();
 
     @GetMapping
-    public ArrayList<Card> getAllCards() {
+    public List<Card> getAllCards() {
         return cardList;
+    }
+
+    @GetMapping("/{id}")
+    public Card getSpecificCard(@PathVariable String id) {
+        Card filteredCard = cardList.stream().filter(
+                (currCard) -> {
+                    return currCard.id.equals(id);
+                }
+        ).findFirst().get();
+        return filteredCard;
     }
 
     @PostMapping
     public Card postCard(@RequestBody Card incomingCard){
+        String idToSet = UUID.randomUUID().toString();
+        if(incomingCard.id != null) {
+            idToSet = incomingCard.id;
+        }
+        incomingCard.id = idToSet;
         cardList.add(incomingCard);
         return incomingCard;
     }
