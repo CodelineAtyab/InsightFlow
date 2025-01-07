9b164ac2810d53adc0146b83a911aea512181014
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-01-07 11:01:19 +0400

    last update (#56)

---
+++ examples/by_alazhar/insight-flow-api/src/test/java/com/insightflow/codearchs/insight_flow_api/InsightFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insightflow.codearchs.insight_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
